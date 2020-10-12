42e13e8364351ea5879ef98b55d0eb9fa0ebfc7c
Guarav Verma <gaurav.verma382001@gmail.com>
2020-10-12 18:04:06 +0530

    Added Fibonnachi Tabulation

---
+++ Dynamic Programming/Fib Memoization.cpp
@@ -0,0 +1,18 @@
+/* 
+
+	TIME COMPLEXITY :  O(N)  where N is the given number 
+	AUXILLARY SPACE :  O(N)	
+*/
+
+int fib(int n){
+
+	int f[n+1];
+	f[0] = 0, f[1] = 1;
+
+	for(int i = 2; i <= n;i++){
+		f[i] = f[i-1] + f[i-2];
+	}
+
+	return f[n];
+}
+
